22ab49efe19e61ff8bf57eaa26de0c368cda3729
Fiona <h.natapra@gmail.com>
2017-01-30 17:28:06 -0500

    html css js initial

---
+++ script.js
@@ -0,0 +1,56 @@
+
+var userAnswer = $('#answer');
+var userInput = $('#input');
+
+var qBank = [
+  {question: " 3 x 15 ? ",
+  answer:45},
+ {question: " 22 x 100 ?",
+ answer: 2200},
+ {question:"33 x 3 ?",
+answer:99,
+question:"33 x 2 ?",
+answer:66,
+question:"25 x 6 ?",
+answer:150,
+}
+];
+
+var next = function() {
+  if (userInput.val() === 30) {
+    document.getElementById('question').innerHTML = qBank[0].question;
+  } else if (userInput.val() !== 30) {
+    alert("Try Again!")
+  };
+    if (userInput.val() == qBank[0].answer) {
+      document.getElementById('question').innerHTML = qBank[1].question;
+    } else if (userInput.val() !== qBank[0].answer) {
+      alert("Try Again!")
+    };
+      if (userInput.val() == qBank[1].answer) {
+        document.getElementById('question').innerHTML = qBank[2].question;
+    }
+      if (userInput.val() == qBank[2].answer) {
+        document.getElementById('question').innerHTML = qBank[3].question;
+      }
+      if (userInput.val() == qBank[3].answer) {
+        document.getElementById('question').innerHTML = qBank[4].question;
+      }
+  }
+
+userAnswer.on("click", function(){
+  next();
+})
+
+/*userAnswer.on("click", function(){
+  if (userInput.val() === "30") {
+    next();
+  }
+})    /*if (userInput.val() === "45")
+    }
+
+
+  } else {
+    alert("Sorry, try again");
+  }
+});*/
