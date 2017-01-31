0affde181f1581bcd4ce1af57eb4b1261a4858a5
Fiona <h.natapra@gmail.com>
2017-01-31 12:35:33 -0500

    second commit

--- project_1/script.js
+++ project_1/script.js
@@ -1,17 +1,42 @@
+/*var kittenOneHolder = $('#kittenOne')
+var kittenTwoHolder = $('#kittenTwo')
+var baconOneHolder = $('#baconOne')*/
+
+
 var getCardOne = $('#cardOne');
 
 getCardOne.on("click",function() {
-  $("#kittenOne").css("display", "block")
+  $("#kittenOne").toggleClass("clicked");
 });
+
 
 var getCardTwo = $('#cardTwo');
 
 getCardTwo.on("click",function() {
-  $("#kittenTwo").css("display", "block")
+  $("#kittenTwo").toggleClass("clicked");
+
 });
 
 var getCardThree = $('#cardThree');
 
 getCardThree.on("click",function() {
-  $("#baconOne").css("display", "block")
+  $("#baconOne").toggleClass("clicked");
+});
+
+var getCardFour = $('#cardFour');
+
+getCardFour.on("click",function() {
+  $("#spaceOne").toggleClass("clicked");
+});
+
+var getCardFive = $('#cardFive');
+
+getCardFive.on("click",function() {
+  $("#baconTwo").toggleClass("clicked");
+});
+
+var getCardSix = $('#cardSix');
+
+getCardSix.on("click",function() {
+  $("#spaceTwo").toggleClass("clicked");
 });
