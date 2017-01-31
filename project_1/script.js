6323beef7d6a1073428148103528212829dddc0f
Fiona <h.natapra@gmail.com>
2017-01-31 10:21:13 -0500

    initial commit flash image

---
+++ project_1/script.js
@@ -0,0 +1,17 @@
+var getCardOne = $('#cardOne');
+
+getCardOne.on("click",function() {
+  $("#kittenOne").css("display", "block")
+});
+
+var getCardTwo = $('#cardTwo');
+
+getCardTwo.on("click",function() {
+  $("#kittenTwo").css("display", "block")
+});
+
+var getCardThree = $('#cardThree');
+
+getCardThree.on("click",function() {
+  $("#baconOne").css("display", "block")
+});
